246ba6e0ab86dc53123cf9b351c8a1dcd7a4966e
mwmMecoy <macmecoy@gmail.com>
2022-03-22 13:59:22 -0500

    code logic for AND, OR, and XOR

---
+++ LogicalCalc.js
@@ -0,0 +1,36 @@
+// Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+
+// Examples
+// booleans = [True, True, False], operator = "AND"
+// True AND True -> True
+// True AND False -> False
+// return False
+// booleans = [True, True, False], operator = "OR"
+// True OR True -> True
+// True OR False -> True
+// return True
+// booleans = [True, True, False], operator = "XOR"
+// True XOR True -> False
+// False XOR False -> False
+// return False
+// Input
+// an array of Boolean values (1 <= array_length <= 50)
+// a string specifying a logical operator: "AND", "OR", "XOR"
+// Output
+// A Boolean value (True or False).
+
+function logicalCalc(array, op){
+    if(op == 'AND') return array.reduce((acc,c) => acc && c);
+    if(op == 'OR') return array.reduce((acc,c) => acc || c);
+    if(op == 'XOR') return array.reduce((acc,c) => acc != c);
+  }
+
+  console.log(logicalCalc([true, true, true, false], "AND"), false);
+  console.log(logicalCalc([true, true, true, false], "OR"), true);
+  console.log(logicalCalc([true, true, true, false], "XOR"), true);
+  console.log(logicalCalc([true, true, false, false], "AND"), false);
+  console.log(logicalCalc([true, true, false, false], "OR"), true);
+  console.log(logicalCalc([true, true, false, false], "XOR"), false);
+  console.log(logicalCalc([true, false, false, false], "AND"), false);
+  console.log(logicalCalc([true, false, false, false], "OR"), true);
+  console.log(logicalCalc([true, false, false, false], "XOR"), true);
