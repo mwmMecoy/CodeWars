b0680950966913944c0090b21f14d0b8fe267d85
mwmMecoy <macmecoy@gmail.com>
2022-06-10 16:28:03 -0500

    completed 52e9aa89b5acdd26d3000127

---
+++ MultiplyCharacters.js
@@ -0,0 +1,18 @@
+// Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+// Expected results:
+
+// spam(1);  // hue
+// spam(6);  // huehuehuehuehuehue
+// spam(14); // huehuehuehuehuehuehuehuehuehuehuehuehuehue
+
+//wrong version
+// function spam(number){
+//     //TODO: Not returning the expected value.
+//     return "hue" * number;
+//   }
+
+  function spam(number){
+    //TODO: Not returning the expected value.
+    return "hue".repeat(number);
+  }
