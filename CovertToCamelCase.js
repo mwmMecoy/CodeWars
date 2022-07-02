cd7c72e2f269e4858c16c1465daa0379e03fd4ec
mwmMecoy <macmecoy@gmail.com>
2022-07-02 15:51:18 -0500

    Solution for 517abf86da9663f1d2000003

---
+++ CovertToCamelCase.js
@@ -0,0 +1,25 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+function toCamelCase(str){
+    console.log(str)
+    if(str.length == 0){
+      return str
+    }
+    str = str.split('')
+    return str.map((x, i) =>{
+      if((x == '-') || (x =='_')){
+        x = str[i+1].toUpperCase();
+        str.splice(i+1, 1)
+      }
+      return x
+    }).join('');
+  }
+
+//   Test.assertEquals(toCamelCase(''), '', "An empty string was provided but not returned")
+// Test.assertEquals(toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+// Test.assertEquals(toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
+// Test.assertEquals(toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value")
