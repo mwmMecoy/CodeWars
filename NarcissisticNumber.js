b17a00ada044fa26d0a15e14d48a41b07ac67b5f
mwmMecoy <macmecoy@gmail.com>
2022-07-22 11:14:42 -0500

    Solution for 5287e858c6b5a9678200083c

--- NarcissisticNumber.js
+++ NarcissisticNumber.js
@@ -12,7 +12,18 @@
 
 // Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
 
-//P:
-//R:
-//E:
-//P:
+//P: getting a single base 10 number. It will be a positive non-zero number, no need to check for text or other invalid inputs.
+//R: boolean, stating whether or not the number given is narcissistic
+//E:For example, take 153 (3 digits), which is narcisstic:
+// 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+//P: convert the number into a string, iterate through the string and set each number to the power of the length of the string then add to a sum variable. Test to see if sum is equal to the given number
+function narcissistic(value) {
+    // Code me to return true or false
+    let strValue = String(value)
+    let sum = 0
+    for(let i = 0 ; i < strValue.length; i++){
+        sum += parseInt(strValue[i])**strValue.length
+    }
+    return sum == value
+}
+  
