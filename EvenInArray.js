26ce11d83c571e0bdefe9be2ebf2b815986cb2a9
mwmMecoy <macmecoy@gmail.com>
2022-10-19 08:05:28 -0500

    Solution for 5a431c0de1ce0ec33a00000c

---
+++ EvenInArray.js
@@ -0,0 +1,14 @@
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+function evenNumbers(array, number) {
+    // filter the array down to only the even numbers
+    array = array.filter(num => num%2===0)
+    // use slice to take the end of the array with the number provided as the argument. Make it negative to start at the back of the array
+    return array.slice(-number)
+  }
